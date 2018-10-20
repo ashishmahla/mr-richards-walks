c03db459fb5731cce69ddb2ae55ac93bd1ed0bea
Ashish Mahla <ashishmahla@users.noreply.github.com>
2018-10-20 08:07:33 +0530

    Update Config.java

--- app/src/main/java/in/scarecrow/mrrichardswalks/Config.java
+++ app/src/main/java/in/scarecrow/mrrichardswalks/Config.java
@@ -1,5 +1,5 @@
 package in.scarecrow.mrrichardswalks;
 
 public class Config {
-    public static final String GOOGLE_MAPS_API_KEY = "AIzaSyDygBXkkcoPgOvQnqSehbHe7YykCYAj6QI";
+    public static final String GOOGLE_MAPS_API_KEY = "AIzaSy---------------------e7YykCYAj6QI";
 }
